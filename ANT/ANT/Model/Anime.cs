86932859e3517fc3ed6630821b8d84c1e515f92d
JaoHundred <43298295+JaoHundred@users.noreply.github.com>
2019-10-06 22:57:59 -0300

    .

---
+++ ANT/ANT/Model/Anime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ANT.Model
+{
+    public class Anime
+    {
+        public string NameEg { get; set; }
+        public string NameJp { get; set; }
+
+        //TODO: ver como se chama o idioma japones em simbolos
+        public string NameJpSym { get; set; }
+    }
+}
---
+++ ANT/ANT/UTIL/NotifyProperty.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ANT.UTIL
+{
+    public class NotifyProperty : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected void OnPropertyChanged<T>(ref T property, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(property, newValue))
+            {
+                property = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
