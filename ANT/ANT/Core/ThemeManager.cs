4cac7895e26bcfc9af8d79960f6b27858a9ef820
JaoHundred <43298295+JaoHundred@users.noreply.github.com>
2019-10-12 18:51:40 -0300

    .

--- ANT/ANT/Core/ThemeManager.cs
+++ ANT/ANT/Core/ThemeManager.cs
@@ -67,5 +67,19 @@ namespace ANT.Core
         {
             return (Themes)CrossSettings.Current.GetValueOrDefault("SelectedTheme", (int)Themes.Light);
         }
+
+        //TODO: descobrir um meio de setar a acent color com o valor do dicionário de chave "PrimaryColor"
+        private void GetAcentColor(Themes themes, ResourceDictionary dic)
+        {
+            switch (themes)
+            {
+                case Themes.Light:
+                    break;
+                case Themes.Dark:
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
