04d6163f99b17c34d938d7377ac91032ebc51f84
JaoHundred <43298295+JaoHundred@users.noreply.github.com>
2020-02-10 16:20:32 -0300

    Remoção de TextReductionConverter e ajustes visuais em CatalogueView, RecentView e AnimeCharacterPopupView para acomodar melhor o texto apresentado.

---
+++ ANT/ANT/_Behaviors/LabelLinesBehavior.cs
@@ -0,0 +1,37 @@
+﻿using Xamarin.Forms;
+
+namespace ANT._Behaviors
+{
+    /// <summary>
+    /// formata a exibição de linhas de texto para aproveitar o espaço(texto comprido recebe Trailtruncate, enquanto texto com espaços ou vírgulas recebem
+    /// o tratamento de duas linhas seguido de trailtruncate)
+    /// </summary>
+    public class LabelLinesBehavior : Behavior<Label>
+    {
+        protected override void OnAttachedTo(Label bindable)
+        {
+            bindable.PropertyChanged += Bindable_PropertyChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        private void Bindable_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Text")
+            {
+                var bindable = (Label)sender;
+                string tx = bindable.Text;
+
+                if (tx == null)
+                    return;
+
+                bindable.MaxLines = tx.Length >= 11 && !tx.Contains(",") && !tx.Contains(" ") ? 1 : 2;
+            }
+        }
+
+        protected override void OnDetachingFrom(Label bindable)
+        {
+            bindable.PropertyChanged -= Bindable_PropertyChanged;
+            base.OnDetachingFrom(bindable);
+        }
+    }
+}
