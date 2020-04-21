e3f67ae14fccd1074a084b289ae5a11a30b1c51a
JaoHundred <43298295+JaoHundred@users.noreply.github.com>
2020-04-21 11:21:23 -0300

    Adicionado conversor para separar o texto dos filtros de gêneros.

---
+++ ANT/ANT/Converter/GenreEnumToSeparateWordConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+using JikanDotNet;
+using System.Threading.Tasks;
+
+namespace ANT.Converter
+{
+    public class GenreEnumToSeparateWordConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            GenreSearch? genre = (GenreSearch)value;
+
+            if (genre != null)
+            {
+                string word = genre.ToString();
+                var builder = new StringBuilder();
+                builder.Append(word[0]);
+
+                for (int i = 1; i < word.Length; i++)
+                {
+                    char c = word[i];
+
+                    if (char.IsUpper(c))
+                        builder.Append(' ');
+
+                    builder.Append(c);
+                }
+
+                return builder.ToString();
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
