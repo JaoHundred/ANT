a30fc3c2e64e3c17ca8387a7d3eb8c54c0c58e8d
JaoHundred <43298295+JaoHundred@users.noreply.github.com>
2020-05-05 19:07:13 -0300

    Implementação inicial de FavoriteAnimeView e FavoriteAnimeViewModel.

--- ANT/ANT/Modules/FavoriteAnime/FavoriteAnimeViewModel.cs
+++ ANT/ANT/Modules/FavoriteAnime/FavoriteAnimeViewModel.cs
@@ -7,6 +7,10 @@ using ANT.UTIL;
 using System.Windows.Input;
 using System.Threading.Tasks;
 using ANT.Interfaces;
+using ANT.Model;
+using ANT.Core;
+using System.Linq;
+using System.Resources;
 
 namespace ANT.Modules
 {
@@ -17,16 +21,57 @@ namespace ANT.Modules
             SearchCommand = new magno.AsyncCommand(OnSearch);
             ClearTextCommand = new magno.AsyncCommand(OnClearText);
             DeleteFavoriteCommand = new magno.AsyncCommand(OnDeleteFavoriteCommand);
+            ClearAllCommand = new magno.AsyncCommand(OnClearAll);
+            SelectionModeCommand = new magno.Command(OnSelectionModeChanged);
         }
 
-        //TODO: implementar esta VM e sua View
+        //TODO: implementar esta VM e sua View(implementar o template da coleção, e mecanismo de switch para ativar ou desativar as notificações
+        //animes que não estão exibindo ou não tem data da semana não vão ter essa opção do switch
         public async Task LoadAsync(object param)
         {
-            await App.DelayRequest();
+            var groupTask = Task.Run(() =>
+            {
+                Lazy<ResourceManager> resMgr = new Lazy<ResourceManager>(
+                    () => new ResourceManager(typeof(Lang.Lang)));
+
+                var group = new List<GroupedFavoriteAnimeByWeekDay>();
+
+                var favorited = App.FavoritedAnimes.Where(p => p.NextStreamDate != null);
+                var favoritedNullDate = App.FavoritedAnimes.Where(p => p.NextStreamDate == null).GroupBy(p => p.NextStreamDate);
+
+                var sortedDaysOfWeek = favorited?.OrderBy(p => p.NextStreamDate.Value.DayOfWeek).GroupBy(p => p.NextStreamDate.Value.DayOfWeek).ToList();
+                var todayAnimes = favorited?.Where(p => p.NextStreamDate.Value.DayOfWeek == DateTime.Today.DayOfWeek)
+                .GroupBy(p => p.NextStreamDate.Value.DayOfWeek);
+
+                if (todayAnimes != null)
+                {
+                    sortedDaysOfWeek.RemoveAll(p => p.Key == DateTime.Today.DayOfWeek);
+                    foreach (var item in todayAnimes)
+                        group.Add(new GroupedFavoriteAnimeByWeekDay(resMgr.Value.GetString(DateTime.Today.DayOfWeek.ToString())
+                        , item.ToList()));
+                }
+               
+                foreach (var anime in sortedDaysOfWeek)
+                    group.Add(new GroupedFavoriteAnimeByWeekDay(resMgr.Value.GetString(anime.Key.ToString()), anime.ToList()));
+                
+                if (favoritedNullDate != null)
+                    foreach (var item in favoritedNullDate)
+                        group.Add(new GroupedFavoriteAnimeByWeekDay(Lang.Lang.UnknownDate, item.ToList()));
+
+                return group;
+            });
+
+
+            GroupedFavoriteByWeekList = new ObservableRangeCollection<GroupedFavoriteAnimeByWeekDay>(await groupTask);
         }
 
         #region Propriedades
-
+        private ObservableRangeCollection<GroupedFavoriteAnimeByWeekDay> _groupedFavoriteByWeekList;
+        public ObservableRangeCollection<GroupedFavoriteAnimeByWeekDay> GroupedFavoriteByWeekList
+        {
+            get { return _groupedFavoriteByWeekList; }
+            set { SetProperty(ref _groupedFavoriteByWeekList, value); }
+        }
         #endregion
 
 
@@ -49,7 +94,40 @@ namespace ANT.Modules
         private async Task OnDeleteFavoriteCommand()
         {
             await App.DelayRequest();
-        } 
+        }
+
+        public ICommand ClearAllCommand { get; private set; }
+        private async Task OnClearAll()
+        {
+            if (GroupedFavoriteByWeekList.Count == 0)
+                return;
+
+            bool canNavigate = await NavigationManager.CanPopUpNavigateAsync<ChoiceModalViewModel>();
+
+            if (canNavigate)
+            {
+                var confirmDelegateAction = new Action(async () =>
+                {
+                    await Task.Run(async () =>
+                    {
+                        App.FavoritedAnimes.Clear();
+                        await JsonStorage.SaveDataAsync(App.FavoritedAnimes, StorageConsts.LocalAppDataFolder
+                            , StorageConsts.FavoritedAnimesFileName);
+                    });
+
+                    GroupedFavoriteByWeekList.Clear();
+                });
+
+                await NavigationManager.
+                    NavigatePopUpAsync<ChoiceModalViewModel>(Lang.Lang.ClearFavoriteList, Lang.Lang.ClearCannotBeUndone, confirmDelegateAction);
+            }
+        }
+
+        public ICommand SelectionModeCommand { get; set; }
+        private void OnSelectionModeChanged()
+        {
+            IsMultiSelect = !IsMultiSelect;
+        }
         #endregion
 
     }
