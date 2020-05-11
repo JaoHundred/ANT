d8cc1a31fac9cfc3db27aae9398b650648d7933e
JaoHundred <43298295+JaoHundred@users.noreply.github.com>
2020-05-11 16:09:22 -0300

    Ajustes nas configurações do projeto.

--- ANT/ANT/AssemblyInfo.cs
+++ ANT/ANT/AssemblyInfo.cs
@@ -1,5 +1,11 @@
+using Android.App;
 using System.Resources;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: NeutralResourcesLanguage("en")]
+#if DEBUG
+[assembly: Application(Debuggable=true)]
+#else
+[assembly: Application(Debuggable = false)]
+#endif
