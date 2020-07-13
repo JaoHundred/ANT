83fb25175a75735d9ca19998e522781c9c63ed20
JaoHundred <43298295+JaoHundred@users.noreply.github.com>
2020-07-13 12:56:40 -0300

    Correção da exibição da data de estreia e término dos animes.

--- ANT/ANT/Converter/DateConverter.cs
+++ ANT/ANT/Converter/DateConverter.cs
@@ -12,7 +12,7 @@ namespace ANT.Converter
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is DateTime date)
-                return date.ToShortDateString();
+                return date.ToUniversalTime().ToShortDateString();
 
             return parameter != null ? null : Consts.NA;
         }
