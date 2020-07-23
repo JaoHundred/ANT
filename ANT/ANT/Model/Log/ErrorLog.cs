8783c64a36823c3c03706eb06b4e02e2d5a9924c
JaoHundred <43298295+JaoHundred@users.noreply.github.com>
2020-07-23 12:44:44 -0300

    Implementação da classe ErrorLog.

---
+++ ANT/ANT/Model/Log/ErrorLog.cs
@@ -0,0 +1,22 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ANT.Model
+{
+    public class ErrorLog
+    {
+        public ErrorLog()
+        {}
+
+        [BsonId(true)]
+        public int Id { get; set; }
+
+        public System.Exception Exception { get; set; }
+
+        public Type ExceptionType { get; set; }
+
+        public DateTime ExceptionDate { get; set; }
+    }
+}
